70b4639cb0670bc72b220ae26c6ef7583d094f3e
richiehsiung <64841079+richiehsiung@users.noreply.github.com>
2020-07-14 16:26:52 -0700

    Create consolescriptv2.js

---
+++ consolescriptv2.js
@@ -0,0 +1,26 @@
+var m = document.querySelectorAll('.PaBnr.eFVJg');
+i=0;
+setInterval (function() {
+    if (i>=m.length) {
+        clearInterval();
+    }
+    var event = new MouseEvent('mouseover', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true
+    });
+    m[i].dispatchEvent(event);
+    m[i].addEventListener('mouseover', function() {
+    console.log('triggered');
+    });
+    let d=document.querySelector('.dCJp8.afkep');
+    d.click();
+    console.log('clicked');
+    let u =document.querySelector('._7UhW9.xLCgt.qyrsm.h_zdq.uL8Hv.M8ipN.T0kll');
+    u.click();
+    console.log('u');
+    let c=document.querySelector('.aOOlW.-Cab_');
+    c.click();
+    console.log('confirmed');
+    i++;
+}, 3200);
